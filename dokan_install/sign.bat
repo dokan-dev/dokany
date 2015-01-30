1cc8883689e244659304466fbf7ebc712fe36d85
Maxime Chamley <maxime.chamley@islog.com>
2015-01-30 13:38:05 +0100

    Add installer sign script

---
+++ dokan_install/sign.bat
@@ -0,0 +1,7 @@
+REM Internal script to sign installer binary before publishing a release on github
+REM This will *NOT* work on your computer without the excepted driver signing certificate!
+
+REM Because of various code signing certificates on our build machines, this script use explicitly a code signing certificate issued by GoDaddy (/i Daddy).
+
+REM Sign exe files
+signtool sign /v /i Daddy /t http://timestamp.verisign.com/scripts/timstamp.dll *.exe
