fca1dd07811b8230b19d3c7e14ce55307b6e4feb
Maxime Chamley <maxime.chamley@islog.com>
2015-02-02 10:17:20 +0100

    Add intermediate certificate in signing process

--- dokan_install/sign.bat
+++ dokan_install/sign.bat
@@ -4,4 +4,4 @@ REM This will *NOT* work on your computer without the excepted driver signing ce
 REM Because of various code signing certificates on our build machines, this script use explicitly a code signing certificate issued by GoDaddy (/i Daddy).
 
 REM Sign exe files
-signtool sign /v /i Daddy /t http://timestamp.verisign.com/scripts/timstamp.dll *.exe
+signtool sign /v /i Daddy /ac "..\cert\Go Daddy Class 2 Certification Authority.cer" /t http://timestamp.verisign.com/scripts/timstamp.dll *.exe
--- sign.bat
+++ sign.bat
@@ -4,10 +4,13 @@ REM This will *NOT* work on your computer without the excepted driver signing ce
 REM Because of various code signing certificates on our build machines, this script use explicitly a code signing certificate issued by GoDaddy (/i Daddy).
 
 REM Sign dll/exe files
-signtool sign /v /i Daddy /t http://timestamp.verisign.com/scripts/timstamp.dll Release\*.dll
-signtool sign /v /i Daddy /t http://timestamp.verisign.com/scripts/timstamp.dll Release\*.exe
+signtool sign /v /i Daddy /ac "cert\Go Daddy Class 2 Certification Authority.cer" /t http://timestamp.verisign.com/scripts/timstamp.dll Release\*.dll
+signtool sign /v /i Daddy /ac "cert\Go Daddy Class 2 Certification Authority.cer" /t http://timestamp.verisign.com/scripts/timstamp.dll Release\*.exe
 
 REM Sign driver files...
-signtool sign /v /i Daddy /t http://timestamp.verisign.com/scripts/timstamp.dll Win7Release\dokan.sys
-signtool sign /v /i Daddy /t http://timestamp.verisign.com/scripts/timstamp.dll Win8Release\dokan.sys
-signtool sign /v /i Daddy /t http://timestamp.verisign.com/scripts/timstamp.dll Win8.1Release\dokan.sys
+signtool sign /v /i Daddy /ac "cert\Go Daddy Class 2 Certification Authority.cer" /t http://timestamp.verisign.com/scripts/timstamp.dll Win7Release\dokan.sys
+signtool sign /v /i Daddy /ac "cert\Go Daddy Class 2 Certification Authority.cer" /t http://timestamp.verisign.com/scripts/timstamp.dll Win8Release\dokan.sys
+signtool sign /v /i Daddy /ac "cert\Go Daddy Class 2 Certification Authority.cer" /t http://timestamp.verisign.com/scripts/timstamp.dll Win8.1Release\dokan.sys
+signtool sign /v /i Daddy /ac "cert\Go Daddy Class 2 Certification Authority.cer" /t http://timestamp.verisign.com/scripts/timstamp.dll x64\Win7Release\dokan.sys
+signtool sign /v /i Daddy /ac "cert\Go Daddy Class 2 Certification Authority.cer" /t http://timestamp.verisign.com/scripts/timstamp.dll x64\Win8Release\dokan.sys
+signtool sign /v /i Daddy /ac "cert\Go Daddy Class 2 Certification Authority.cer" /t http://timestamp.verisign.com/scripts/timstamp.dll x64\Win8.1Release\dokan.sys
