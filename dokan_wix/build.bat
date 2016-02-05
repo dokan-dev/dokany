7d198060d4ab4b0e5fdd4f457140f29b3c4f48d5
Adrien JUND <adrien.jund@islog.com>
2016-02-05 14:08:18 +0100

    Add Wix installer - Ty to @js69

---
+++ dokan_wix/build.bat
@@ -0,0 +1,21 @@
+set PATH=%PATH%;%PROGRAMFILES(x86)%\MSBuild\14.0\Bin
+set VCTargetsPath=%PROGRAMFILES%
+IF %processor_architecture%==AMD64 set VCTargetsPath=%PROGRAMFILES(x86)%
+set VCTargetsPath=%VCTargetsPath%\MSBuild\Microsoft.Cpp\v4.0\V140
+
+REM build light installer
+powershell -Command "(gc version.xml) -replace 'yes', 'no' | sc version.xml"
+msbuild Dokan_WiX.sln /p:Configuration=Release /p:Platform="Mixed Platforms" /t:rebuild /fileLogger
+copy Bootstrapper\bin\Release\DokanSetup.exe .
+msbuild Dokan_WiX.sln /p:Configuration=Debug /p:Platform="Mixed Platforms" /t:rebuild /fileLogger
+copy Bootstrapper\bin\Debug\DokanSetup.exe DokanSetupDbg.exe
+
+REM build full installer
+powershell -Command "(gc version.xml) -replace 'no', 'yes' | sc version.xml"
+msbuild Dokan_WiX.sln /p:Configuration=Release /p:Platform="Mixed Platforms" /t:rebuild /fileLogger
+copy Bootstrapper\bin\Release\DokanSetup.exe DokanSetup_redist.exe
+msbuild Dokan_WiX.sln /p:Configuration=Debug /p:Platform="Mixed Platforms" /t:rebuild /fileLogger
+copy Bootstrapper\bin\Debug\DokanSetup.exe DokanSetupDbg_redist.exe
+
+REM build archive
+"C:\Program Files\7-Zip\7z.exe" a -tzip dokan.zip ../Win32 ../x64
--- sign.bat
+++ sign.bat
@@ -1,20 +1,36 @@
 REM Internal script to sign binaries before publishing a release on github
 REM This will *NOT* work on your computer without the excepted driver signing certificate!
 
-REM Because of various code signing certificates on our build machines, this script use explicitly a code signing certificate issued by GoDaddy (/i Daddy).
+REM Because of various code signing certificates on our build machines, this script use explicitly a code signing certificate issued by Go"%CERTISSUER%" (/i "%CERTISSUER%").
 
-REM Sign dll/exe files
-signtool sign /fd SHA256 /v /i Daddy /ac "cert\Go Daddy Root Certification Authority - G2.cer" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Release\*.dll
-signtool sign /fd SHA256 /v /i Daddy /ac "cert\Go Daddy Root Certification Authority - G2.cer" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Release\*.exe
-signtool sign /fd SHA256 /v /i Daddy /ac "cert\Go Daddy Root Certification Authority - G2.cer" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Release\*.dll
-signtool sign /fd SHA256 /v /i Daddy /ac "cert\Go Daddy Root Certification Authority - G2.cer" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Release\*.exe
+REM Sign Release dll/exe files
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Release\*.dll
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Release\*.exe
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Release\*.dll
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Release\*.exe
 
-REM Sign driver files...
-signtool sign /fd SHA256 /v /i Daddy /ac "cert\Go Daddy Root Certification Authority - G2.cer" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Win7Release\*.sys
-signtool sign /fd SHA256 /v /i Daddy /ac "cert\Go Daddy Root Certification Authority - G2.cer" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Win8Release\*.sys
-signtool sign /fd SHA256 /v /i Daddy /ac "cert\Go Daddy Root Certification Authority - G2.cer" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Win8.1Release\*.sys
-signtool sign /fd SHA256 /v /i Daddy /ac "cert\Go Daddy Root Certification Authority - G2.cer" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Win10Release\*.sys
-signtool sign /fd SHA256 /v /i Daddy /ac "cert\Go Daddy Root Certification Authority - G2.cer" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Win7Release\*.sys
-signtool sign /fd SHA256 /v /i Daddy /ac "cert\Go Daddy Root Certification Authority - G2.cer" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Win8Release\*.sys
-signtool sign /fd SHA256 /v /i Daddy /ac "cert\Go Daddy Root Certification Authority - G2.cer" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Win8.1Release\*.sys
-signtool sign /fd SHA256 /v /i Daddy /ac "cert\Go Daddy Root Certification Authority - G2.cer" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Win10Release\*.sys
+REM Sign Release driver files...
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Win7Release\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Win8Release\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Win8.1Release\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Win10Release\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Win7Release\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Win8Release\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Win8.1Release\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Win10Release\*.sys
+
+REM Sign Debug dll/exe files
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Debug\*.dll
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Debug\*.exe
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Debug\*.dll
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Debug\*.exe
+
+REM Sign Debug driver files...
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Win7Debug\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Win8Debug\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Win8.1Debug\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 Win32\Win10Debug\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Win7Debug\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Win8Debug\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Win8.1Debug\*.sys
+signtool sign /fd SHA256 /v /i "%CERTISSUER%" /ac "%ADDITIONALCERT%" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 x64\Win10Debug\*.sys
