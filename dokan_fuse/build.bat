20927c29ed80d5218fbc0a693211630759abcc61
Adrien JUND <liryna.stark@gmail.com>
2016-03-06 11:07:07 -0800

    Add msys2 fuse binaries to installer

--- dokan_fuse/build.bat
+++ dokan_fuse/build.bat
@@ -9,3 +9,15 @@ C:\cygwin64\bin\bash -lc "cd '%cd%'/dokan_fuse/ && cmake . && make"
 C:\cygwin64\bin\bash -lc "cd '%cd%'/samples/fuse_mirror/ && gcc -o mirror fusexmp.c -I../../dokan_fuse/include -D_FILE_OFFSET_BITS=64 -L../../dokan_fuse -lcygdokanfuse1"
 echo f | xcopy /f /y "dokan_fuse/cygdokanfuse1.dll" "x64/Cygwin/"
 echo f | xcopy /f /y "samples/fuse_mirror/mirror.exe" "x64/Cygwin/"
+
+
+
+call dokan_fuse/cleancmake.bat
+START C:\msys64\mingw32_shell.bat -lc "cd '%cd%'/dokan_fuse/ && cmake . -G 'MSYS Makefiles' && make"
+timeout /t 30
+echo f | xcopy /f /y "dokan_fuse/libdokanfuse1.dll" "Win32/Msys2/"
+
+call dokan_fuse/cleancmake.bat
+START C:\msys64\mingw64_shell.bat -lc "cd '%cd%'/dokan_fuse/ && cmake . -G 'MSYS Makefiles' && make"
+timeout /t 30
+echo f | xcopy /f /y "dokan_fuse/libdokanfuse1.dll" "x64/Msys2/"
