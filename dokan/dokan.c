e43052006c96bc440dc9b6481b3863aac30bc9da
Maxhy <maxime@islog.com>
2016-06-04 19:20:09 +0200

    Create KeepAlive and Loop threads before application notification

--- dokan/dokan.c
+++ dokan/dokan.c
@@ -270,25 +270,6 @@ int DOKANAPI DokanMain(PDOKAN_OPTIONS DokanOptions,
     return DOKAN_START_ERROR;
   }
 
-  if (!DokanMount(instance->MountPoint, instance->DeviceName, DokanOptions)) {
-    SendReleaseIRP(instance->DeviceName);
-    DokanDbgPrint("Dokan Error: DokanMount Failed\n");
-    CloseHandle(device);
-    return DOKAN_MOUNT_ERROR;
-  }
-
-  // Here we should have been mounter by mountmanager thanks to
-  // IOCTL_MOUNTDEV_QUERY_SUGGESTED_LINK_NAME
-  DbgPrintW(L"mounted: %s -> %s\n", instance->MountPoint, instance->DeviceName);
-
-  if (DokanOperations->Mounted) {
-    DOKAN_FILE_INFO fileInfo;
-    RtlZeroMemory(&fileInfo, sizeof(DOKAN_FILE_INFO));
-    fileInfo.DokanOptions = DokanOptions;
-    // ignore return value
-    DokanOperations->Mounted(&fileInfo);
-  }
-
   // Start Keep Alive thread
   threadIds[threadNum++] = (HANDLE)_beginthreadex(NULL, // Security Attributes
                                                   0,    // stack size
@@ -304,6 +285,25 @@ int DOKANAPI DokanMain(PDOKAN_OPTIONS DokanOptions,
                                                     (PVOID)instance, // param
                                                     0, // create flag
                                                     NULL);
+  }
+
+  if (!DokanMount(instance->MountPoint, instance->DeviceName, DokanOptions)) {
+	  SendReleaseIRP(instance->DeviceName);
+	  DokanDbgPrint("Dokan Error: DokanMount Failed\n");
+	  CloseHandle(device);
+	  return DOKAN_MOUNT_ERROR;
+  }
+
+  // Here we should have been mounter by mountmanager thanks to
+  // IOCTL_MOUNTDEV_QUERY_SUGGESTED_LINK_NAME
+  DbgPrintW(L"mounted: %s -> %s\n", instance->MountPoint, instance->DeviceName);
+
+  if (DokanOperations->Mounted) {
+	  DOKAN_FILE_INFO fileInfo;
+	  RtlZeroMemory(&fileInfo, sizeof(DOKAN_FILE_INFO));
+	  fileInfo.DokanOptions = DokanOptions;
+	  // ignore return value
+	  DokanOperations->Mounted(&fileInfo);
   }
 
   // wait for thread terminations
