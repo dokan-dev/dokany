359189f0da4477d684f2dbcf9e1624ef98797e25
Liryna <liryna.stark@gmail.com>
2016-06-11 12:17:01 +0200

    Appveyor - Check msbuild succeeded / Move to new Msys2 / Improve cygwin install / Add logs fuse build script / Remove warning from cleancmake and Xamarin

--- dokan_fuse/build.ps1
+++ dokan_fuse/build.ps1
@@ -1,6 +1,7 @@
 $currentPath=Get-Location
 
 & dokan_fuse/cleancmake.bat
+Write-Host "Build Cygwin x86..."
 & C:\cygwin\bin\bash -lc "cd '$currentPath'/dokan_fuse/ && cmake . && make"
 & C:\cygwin\bin\bash -lc "cd '$currentPath'/samples/fuse_mirror/ && gcc -o mirror fusexmp.c -I../../dokan_fuse/include -D_FILE_OFFSET_BITS=64 -L../../dokan_fuse -lcygdokanfuse1"
 New-Item -Force -Type Directory "Win32/Cygwin/"
@@ -8,8 +9,10 @@ copy-item "dokan_fuse/cygdokanfuse1.dll" "Win32/Cygwin/"
 copy-item "samples/fuse_mirror/mirror.exe" "Win32/Cygwin/" 
 Remove-Item "dokan_fuse/cygdokanfuse1.dll"
 Remove-Item "samples/fuse_mirror/mirror.exe"
+Write-Host "Build Cygwin x86 - Done" -ForegroundColor Green
 
 & dokan_fuse/cleancmake.bat
+Write-Host "Build Cygwin x64..."
 & C:\cygwin64\bin\bash -lc "cd '$currentPath'/dokan_fuse/ && cmake . && make"
 & C:\cygwin64\bin\bash -lc "cd '$currentPath'/samples/fuse_mirror/ && gcc -o mirror fusexmp.c -I../../dokan_fuse/include -D_FILE_OFFSET_BITS=64 -L../../dokan_fuse -lcygdokanfuse1"
 New-Item -Force -Type Directory "x64/Cygwin/"
@@ -17,15 +20,20 @@ copy-item "dokan_fuse/cygdokanfuse1.dll" "x64/Cygwin/"
 copy-item "samples/fuse_mirror/mirror.exe" "x64/Cygwin/" 
 Remove-Item "dokan_fuse/cygdokanfuse1.dll"
 Remove-Item "samples/fuse_mirror/mirror.exe"
+Write-Host "Build Cygwin x64 - Done" -ForegroundColor Green
 
 & dokan_fuse/cleancmake.bat
-& C:\msys64\mingw32_shell.bat -lc "cd '$currentPath'/dokan_fuse/ && cmake . -G 'MSYS Makefiles' && make" | Out-Host
+Write-Host "Build Msys2 x86..."
+& C:\msys64\msys2_shell.cmd -mingw32 -lc "cd '$currentPath'/dokan_fuse/ && cmake . -G 'MSYS Makefiles' && make" | Out-Host
 New-Item -Force -Type Directory "Win32/Msys2/"
 copy-item "dokan_fuse/libdokanfuse1.dll" "Win32/Msys2/"
 Remove-Item "dokan_fuse/libdokanfuse1.dll"
+Write-Host "Build Msys2 x86 - Done" -ForegroundColor Green
 
 & dokan_fuse/cleancmake.bat
-& C:\msys64\mingw64_shell.bat -lc "cd '$currentPath'/dokan_fuse/ && cmake . -G 'MSYS Makefiles' && make" | Out-Host
+Write-Host "Build Msys2 x64..."
+& C:\msys64\msys2_shell.cmd -mingw64 -lc "cd '$currentPath'/dokan_fuse/ && cmake . -G 'MSYS Makefiles' && make" | Out-Host
 New-Item -Force -Type Directory "x64/Msys2/"
 copy-item "dokan_fuse/libdokanfuse1.dll" "x64/Msys2/"
 Remove-Item "dokan_fuse/libdokanfuse1.dll"
+Write-Host "Build Msys2 x64 - Done" -ForegroundColor Green
--- dokan_fuse/cleancmake.bat
+++ dokan_fuse/cleancmake.bat
@@ -1,2 +1,3 @@
-del CMakeCache.txt /Q /S
+@echo off
+del CMakeCache.txt /Q /S >nul 2>&1
 for /d /r . %%d in (CMakeFiles) do @if exist "%%d" rd /s/q "%%d"
