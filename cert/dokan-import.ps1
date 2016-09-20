2f16f66548a25934e4a668cc363d3a445e5dbb21
Adrien JUND <adrien.jund@islog.com>
2016-09-20 14:52:18 +0200

    Dokan cert import - check if certmgr is installed

--- cert/dokan-import.ps1
+++ cert/dokan-import.ps1
@@ -1,3 +1,10 @@
+if (!(Get-Command "CertMgr" -errorAction SilentlyContinue))
+{
+	Write-Host "CertMgr (Certificate Manager Tool)does not seem to be installed on your system." -ForegroundColor Red
+	Write-Host "The Certificate Manager is automatically installed with Visual Studio." -ForegroundColor Red
+	return;
+}
+
 Import-Certificate cert\DokanCA.cer -CertStoreLocation "Cert:\LocalMachine\Root"
 Import-Certificate cert\DokanCA.cer -CertStoreLocation "Cert:\LocalMachine\TrustedPublisher"
 Import-PfxCertificate .\cert\DokanSign.pfx  -CertStoreLocation "Cert:\CurrentUser\my"
