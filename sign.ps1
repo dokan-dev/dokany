56995eda7b78203c3cb1cf282ac80867dd41cfd8
Adrien JUND <adrien.jund@islog.com>
2017-03-10 17:12:03 +0100

    Installer - Add EV signtool cmd

--- sign.ps1
+++ sign.ps1
@@ -1,7 +1,9 @@
-$files = Get-ChildItem -path Win32,x64,ARM -recurse -Include *.sys,*.cat,*.dll
-
+$files = Get-ChildItem -path Win32,x64,ARM -recurse -Include *.sys,*.cat,*.dlls
 signtool sign /v /i "$env:CERTISSUER" /ac "$env:ADDITIONALCERT" /t http://timestamp.verisign.com/scripts/timstamp.dll $files
 signtool sign /as /fd SHA256 /v /i "$env:CERTISSUER" /ac "$env:ADDITIONALCERT" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 $files
 
-
-
+if (-not (Test-Path $env:EV_CERTISSUER))
+{
+	$files = Get-ChildItem -path Win32\Win10Release,x64\Win10Release -recurse -Include *.sys,*.cat,*.dll
+	signtool sign /as /fd sha256 /tr http://timestamp.digicert.com /td sha256 /n "$env:EV_CERTISSUER" $files
+}
