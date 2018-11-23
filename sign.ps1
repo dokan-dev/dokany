0fdaad0cfa87cf1052d4d2a91c718789b127d8eb
Liryna <liryna.stark@gmail.com>
2018-11-23 22:33:00 +0100

    Sign - add EV informations

--- sign.ps1
+++ sign.ps1
@@ -8,6 +8,7 @@ $files = Get-ChildItem -path Win32,x64,ARM,ARM64 -recurse -Include *.sys,*.cat,*
 signtool sign /v /i "$env:CERTISSUER" /ac "$env:ADDITIONALCERT" /t http://timestamp.verisign.com/scripts/timstamp.dll $files
 signtool sign /as /fd SHA256 /v /i "$env:CERTISSUER" /ac "$env:ADDITIONALCERT" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 $files
 
+# Need to sign with EV cert for Win10 before requesting Microsoft sign on dev hardware platform
 if (-not ([string]::IsNullOrEmpty($env:EV_CERTISSUER)))
 {
 	New-Item -ItemType Directory -Force -Path Win32\Win10Release,x64\Win10Release,ARM\Win10Release,ARM64\Win10Release,Win32\Win10Debug,x64\Win10Debug,ARM\Win10Debug,ARM64\Win10Debug | Out-Null
