5d5c7bf9967ada0189127f49028962ed5852e951
Adrien JUND <liryna.stark@gmail.com>
2019-01-02 13:13:27 +0100

    Update Copyright 2019

--- dokan/dokan.h
+++ dokan/dokan.h
@@ -1,7 +1,7 @@
 /*
   Dokan : user-mode file system library for Windows
 
-  Copyright (C) 2015 - 2018 Adrien J. <liryna.stark@gmail.com> and Maxime C. <maxime@islog.com>
+  Copyright (C) 2015 - 2019 Adrien J. <liryna.stark@gmail.com> and Maxime C. <maxime@islog.com>
   Copyright (C) 2007 - 2011 Hiroki Asakawa <info@dokan-dev.net>
 
   http://dokan-dev.github.io
