f7161d7f760f8bf3597c0597066192363b4d470c
Adrien JUND <adrienj@google.com>
2020-07-21 16:00:18 +0200

    Copyright - Add and update the missing ones

--- dokan/setfile.c
+++ dokan/setfile.c
@@ -1,6 +1,7 @@
 /*
   Dokan : user-mode file system library for Windows
 
+  Copyright (C) 2020 Google, Inc.
   Copyright (C) 2015 - 2019 Adrien J. <liryna.stark@gmail.com> and Maxime C. <maxime@islog.com>
   Copyright (C) 2007 - 2011 Hiroki Asakawa <info@dokan-dev.net>
 
