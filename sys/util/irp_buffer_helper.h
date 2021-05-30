6c9f700cdf4c9cffa4e61acb936b521b132f6594
Liryna <liryna.stark@gmail.com>
2021-05-30 14:49:55 +0200

    Kernel - Fix break exit type no longer having the wanted effect
    Partial revert ffd04be86050b544a5132846b7b85874ef299ac5

--- sys/util/irp_buffer_helper.h
+++ sys/util/irp_buffer_helper.h
@@ -36,7 +36,7 @@ PVOID GetInputBuffer(_In_ PIRP Irp);
 
 #define GET_IRP_GENERIC_BUFFER_EX(Irp, Buffer, SizeCompare, Exit, Status,    \
                                   InformationSize)                           \
-  do {                                                                       \
+  {                                                                          \
     ULONG irpBufferLen = GetProvidedInputSize(Irp);                          \
     (Buffer) = GetInputBuffer(Irp);                                          \
     ASSERT((Buffer) != NULL);                                                \
@@ -47,7 +47,7 @@ PVOID GetInputBuffer(_In_ PIRP Irp);
       (Buffer) = NULL;                                                       \
       Exit((Irp), (Status), (InformationSize));                              \
     }                                                                        \
-  } while (0)
+  }
 
 #define GET_IRP_GENERIC_BUFFER(Irp, Buffer, CompareSize) \
   GET_IRP_GENERIC_BUFFER_EX(Irp, Buffer, CompareSize, DOKAN_EXIT_NONE, 0, 0)
