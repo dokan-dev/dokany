b74b6484b3e6f872d7a7eeac2b9ecf683b653b71
Liryna <liryna.stark@gmail.com>
2024-01-04 21:01:27 -0500

    Kernel - Fix BSOD when pulling events while the volume mount did not happen

--- sys/fscontrol.c
+++ sys/fscontrol.c
@@ -528,7 +528,7 @@ NTSTATUS DokanProcessAndPullEvents(__in PREQUEST_CONTEXT RequestContext) {
     DOKAN_LOG_FINE_IRP(RequestContext, "No output buffer provided");
     return status;
   }
-  if (IsUnmountPendingVcb(RequestContext->Vcb)) {
+  if (RequestContext->Vcb == NULL || IsUnmountPendingVcb(RequestContext->Vcb)) {
     return STATUS_NO_SUCH_DEVICE;
   }
   // 3 - Flag the device as having workers starting to pull events.
