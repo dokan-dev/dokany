415ac366eaa7a257ceb217e79dab9f37fadc7d33
Liryna <liryna.stark@gmail.com>
2024-02-04 09:32:18 -0500

    Install - Copy msi to choco package

--- chocolatey/package.ps1
+++ chocolatey/package.ps1
@@ -7,6 +7,8 @@ Remove-Item .\chocolatey\build -Recurse -Force -ErrorAction SilentlyContinue
 New-Item .\chocolatey\build -Force -ItemType Directory
 Copy-Item .\chocolatey\dokany.nuspec.template .\chocolatey\build\dokany.nuspec
 Copy-Item .\chocolatey\tools .\chocolatey\build\ -Recurse
+Copy-Item .\dokan_wix\Dokan_x64.msi .\chocolatey\build\tools\Dokan_x64.msi
+Copy-Item .\dokan_wix\Dokan_x86.msi .\chocolatey\build\tools\Dokan_x86.msi
 Rename-Item .\chocolatey\build\tools\chocolateyinstall.ps1.template chocolateyinstall.ps1
 
 (Get-Content -Encoding UTF8 .\chocolatey\build\dokany.nuspec).Replace('[[PackageVersion]]', $version) | Set-Content -Encoding UTF8 .\chocolatey\build\dokany.nuspec
@@ -20,8 +22,8 @@ function Hash {
     return $hash.Hash
 }
 
-$hash64 = Hash .\dokan_wix\Dokan_x64.msi
-$hash32 = Hash .\dokan_wix\Dokan_x86.msi
+$hash64 = Hash .\chocolatey\build\tools\Dokan_x64.msi
+$hash32 = Hash .\chocolatey\build\tools\Dokan_x86.msi
 
 $install = (Get-Content .\chocolatey\build\tools\chocolateyinstall.ps1)
 $install = $install.Replace('[[Url]]', $url32).Replace('[[Checksum]]', $hash32)
