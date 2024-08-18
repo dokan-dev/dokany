252d324208a57ddafcdc62553626f96835a73b88
Liryna <liryna.stark@gmail.com>
2024-08-18 11:14:40 -0400

    Choco - Take the API key from env

--- chocolatey/release.ps1
+++ chocolatey/release.ps1
@@ -4,4 +4,4 @@ $ErrorActionPreference = 'Stop'
 
 .\chocolatey\package.ps1 $version
 
-choco push ".\chocolatey\build\dokany2.${version}.nupkg" --api-key="${CHOCO_API_KEY}" --source="https://push.chocolatey.org/"
+choco push ".\chocolatey\build\dokany2.${version}.nupkg" --api-key="$env:CHOCO_API_KEY" --source="https://push.chocolatey.org/"
